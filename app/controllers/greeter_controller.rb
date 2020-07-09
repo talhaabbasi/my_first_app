42de8b90a81638688ec9f119a488c565c75de7a0
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-07-09 12:11:35 +0500

    Helpers added

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Talha","Hasher","Usama","Sabih"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1 
   end
   def goodbye
   end
